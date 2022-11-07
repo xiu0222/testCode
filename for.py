99acdebebdfad68f3678b60d6c0c9c29edecc57b
wangxiu0222 <wangxiu0222@163.com>
2022-11-08 00:18:05 +0800

    for循环

---
+++ for.py
@@ -0,0 +1,14 @@
+#循环
+"""
+# chengji = [80,90,59,49,28,]
+# for i in chengji:
+#     if i < 60 :  for的in去取值
+#         print("发现一个壮士，成绩是{}".format(i))
+"""
+
+"""
+chengji = [80,90,59,49,28,]
+for i in chengji:
+    if i < 60 :
+        print(i)
+"""
