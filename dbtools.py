5ee0a5966705076545b39e42479ed252f8913e27
wangxiu0222 <wangxiu0222@163.com>
2022-11-11 19:48:26 +0800

    第一次

--- dbtools.py
+++ dbtools.py
@@ -1,2 +1,44 @@
-import   pymysql
-# 想要用朋友MySQL,就必须要导入它
+"""
+# import pymysql  # 想要用朋友MySQL,就必须要导入它
+
+
+
+def query(sql):
+    # 固定的方法
+    #    db = pymysql.connect(host='ip地址',user='账号',password="密码 ",db='数据库名字') 
+    db = pymysql.connect(host='192.144.148.91',user='ljtest',password="lj123456+",db='ljtest') 
+query("123")
+#获取查询窗口，游标： 
+    cur = db.cursor()
+#执行SQL语句
+    cur.execute(sql)
+#获取所有的结果
+    res = cur.fetchall()
+    db.close()
+    return res
+
+
+
+if _name_ = "_main_":
+    # sql = "select * from t_user where username ='liuyun'"
+    sql ="select * from t_pymysql_account wherer username = 'python'"
+    a = query(sql)
+    print(a) 
+
+
+
+
+"""
+
+
+
+
+
+
+sql = "selsct * from t_goods where goods = 'iphone12'"
+res = query(sql)
+if res [0][2] >5488:
+    print("打扰了，我选华为 ")
+else:
+    print("我爱华为，我选苹果")
+    
--- method.py
+++ method.py
@@ -1,13 +1,5 @@
 
-
-
-
-
-
-
-
-
-
+"""
 # 练习自定义一个减法方法/函数，要求去调用这个方法,显示返回值
 def add(s1,s2):
      sum=s1-s2
@@ -20,8 +12,28 @@ b = add(8,5)
 print(a)
 print(b)
 
+"""
 
+def login(username,password):
 
+    t_user = {"username":"郭子","passward":"123456"},{"username":"小玉","passward":"123456"}
+    u = input("请输入账号:")
+    p = input("请输入密码:")
+    a = 1
+    for i in t_user:
+        if u == i.get("username") and p == i.get("passward"):
+            print("登陆成功")
+            break   #终止循环
+        else:
+        # 最后一次运行还没有这个账号：再来打印登陆失败
+        # 怎么判断是最后一次
+            if len(t_user) == a:
+                print("登陆失败 ")
 
+    a = a + 1
+
+u = input("请输入账号：")
+p = input("请输入密码：")
+login(u,p)
 
 
